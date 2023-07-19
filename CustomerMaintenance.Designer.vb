296e8b96878216e5c2a037816bf60d9efdb70164
Aziz <azizahmed.dev@gmail.com>
2023-07-18 23:43:04 -0400

    Add project files.

---
+++ CustomerMaintenance.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CustomerMaintenance
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CustomerMaintenance))
+        Dim CustomerIDLabel As System.Windows.Forms.Label
+        Dim NameLabel As System.Windows.Forms.Label
+        Dim AddressLabel As System.Windows.Forms.Label
+        Dim CityLabel As System.Windows.Forms.Label
+        Me.MMABooksDataSet = New ahmed_exc_14_2.MMABooksDataSet()
+        Me.CustomersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CustomersTableAdapter = New ahmed_exc_14_2.MMABooksDataSetTableAdapters.CustomersTableAdapter()
+        Me.TableAdapterManager = New ahmed_exc_14_2.MMABooksDataSetTableAdapters.TableAdapterManager()
+        Me.CustomersBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.CustomersBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.CustomerIDTextBox = New System.Windows.Forms.TextBox()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.AddressTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
+        CustomerIDLabel = New System.Windows.Forms.Label()
+        NameLabel = New System.Windows.Forms.Label()
+        AddressLabel = New System.Windows.Forms.Label()
+        CityLabel = New System.Windows.Forms.Label()
+        CType(Me.MMABooksDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomersBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.CustomersBindingNavigator.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MMABooksDataSet
+        '
+        Me.MMABooksDataSet.DataSetName = "MMABooksDataSet"
+        Me.MMABooksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'CustomersBindingSource
+        '
+        Me.CustomersBindingSource.DataMember = "Customers"
+        Me.CustomersBindingSource.DataSource = Me.MMABooksDataSet
+        '
+        'CustomersTableAdapter
+        '
+        Me.CustomersTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CustomersTableAdapter = Me.CustomersTableAdapter
+        Me.TableAdapterManager.InvoiceLineItemsTableAdapter = Nothing
+        Me.TableAdapterManager.InvoicesTableAdapter = Nothing
+        Me.TableAdapterManager.OrderOptionsTableAdapter = Nothing
+        Me.TableAdapterManager.ProductsTableAdapter = Nothing
+        Me.TableAdapterManager.StatesTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = ahmed_exc_14_2.MMABooksDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'CustomersBindingNavigator
+        '
+        Me.CustomersBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.CustomersBindingNavigator.BindingSource = Me.CustomersBindingSource
+        Me.CustomersBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.CustomersBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.CustomersBindingNavigator.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.CustomersBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.CustomersBindingNavigatorSaveItem})
+        Me.CustomersBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.CustomersBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.CustomersBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.CustomersBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.CustomersBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.CustomersBindingNavigator.Name = "CustomersBindingNavigator"
+        Me.CustomersBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.CustomersBindingNavigator.Size = New System.Drawing.Size(528, 27)
+        Me.CustomersBindingNavigator.TabIndex = 0
+        Me.CustomersBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(29, 22)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(29, 22)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 27)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(45, 20)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(29, 24)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(29, 24)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(29, 24)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(29, 24)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'CustomersBindingNavigatorSaveItem
+        '
+        Me.CustomersBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.CustomersBindingNavigatorSaveItem.Image = CType(resources.GetObject("CustomersBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.CustomersBindingNavigatorSaveItem.Name = "CustomersBindingNavigatorSaveItem"
+        Me.CustomersBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 23)
+        Me.CustomersBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'CustomerIDLabel
+        '
+        CustomerIDLabel.AutoSize = True
+        CustomerIDLabel.Location = New System.Drawing.Point(27, 49)
+        CustomerIDLabel.Name = "CustomerIDLabel"
+        CustomerIDLabel.Size = New System.Drawing.Size(89, 17)
+        CustomerIDLabel.TabIndex = 1
+        CustomerIDLabel.Text = "Customer ID:"
+        '
+        'CustomerIDTextBox
+        '
+        Me.CustomerIDTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "CustomerID", True))
+        Me.CustomerIDTextBox.Location = New System.Drawing.Point(136, 46)
+        Me.CustomerIDTextBox.Name = "CustomerIDTextBox"
+        Me.CustomerIDTextBox.ReadOnly = True
+        Me.CustomerIDTextBox.Size = New System.Drawing.Size(132, 22)
+        Me.CustomerIDTextBox.TabIndex = 2
+        '
+        'NameLabel
+        '
+        NameLabel.AutoSize = True
+        NameLabel.Location = New System.Drawing.Point(27, 77)
+        NameLabel.Name = "NameLabel"
+        NameLabel.Size = New System.Drawing.Size(49, 17)
+        NameLabel.TabIndex = 3
+        NameLabel.Text = "Name:"
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "Name", True))
+        Me.NameTextBox.Location = New System.Drawing.Point(136, 74)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(353, 22)
+        Me.NameTextBox.TabIndex = 4
+        '
+        'AddressLabel
+        '
+        AddressLabel.AutoSize = True
+        AddressLabel.Location = New System.Drawing.Point(27, 105)
+        AddressLabel.Name = "AddressLabel"
+        AddressLabel.Size = New System.Drawing.Size(64, 17)
+        AddressLabel.TabIndex = 5
+        AddressLabel.Text = "Address:"
+        '
+        'AddressTextBox
+        '
+        Me.AddressTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "Address", True))
+        Me.AddressTextBox.Location = New System.Drawing.Point(136, 102)
+        Me.AddressTextBox.Name = "AddressTextBox"
+        Me.AddressTextBox.Size = New System.Drawing.Size(353, 22)
+        Me.AddressTextBox.TabIndex = 6
+        '
+        'CityLabel
+        '
+        CityLabel.AutoSize = True
+        CityLabel.Location = New System.Drawing.Point(27, 133)
+        CityLabel.Name = "CityLabel"
+        CityLabel.Size = New System.Drawing.Size(104, 17)
+        CityLabel.TabIndex = 7
+        CityLabel.Text = "City, State, Zip:"
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "City", True))
+        Me.CityTextBox.Location = New System.Drawing.Point(136, 130)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.Size = New System.Drawing.Size(202, 22)
+        Me.CityTextBox.TabIndex = 8
+        '
+        'StateTextBox
+        '
+        Me.StateTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "State", True))
+        Me.StateTextBox.Location = New System.Drawing.Point(344, 130)
+        Me.StateTextBox.Name = "StateTextBox"
+        Me.StateTextBox.Size = New System.Drawing.Size(34, 22)
+        Me.StateTextBox.TabIndex = 10
+        '
+        'ZipCodeTextBox
+        '
+        Me.ZipCodeTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "ZipCode", True))
+        Me.ZipCodeTextBox.Location = New System.Drawing.Point(384, 130)
+        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
+        Me.ZipCodeTextBox.Size = New System.Drawing.Size(105, 22)
+        Me.ZipCodeTextBox.TabIndex = 12
+        '
+        'CustomerMaintenance
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 177)
+        Me.Controls.Add(CustomerIDLabel)
+        Me.Controls.Add(Me.CustomerIDTextBox)
+        Me.Controls.Add(NameLabel)
+        Me.Controls.Add(Me.NameTextBox)
+        Me.Controls.Add(AddressLabel)
+        Me.Controls.Add(Me.AddressTextBox)
+        Me.Controls.Add(CityLabel)
+        Me.Controls.Add(Me.CityTextBox)
+        Me.Controls.Add(Me.StateTextBox)
+        Me.Controls.Add(Me.ZipCodeTextBox)
+        Me.Controls.Add(Me.CustomersBindingNavigator)
+        Me.Name = "CustomerMaintenance"
+        Me.Text = "Customer Maintenance"
+        CType(Me.MMABooksDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomersBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.CustomersBindingNavigator.ResumeLayout(False)
+        Me.CustomersBindingNavigator.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MMABooksDataSet As MMABooksDataSet
+    Friend WithEvents CustomersBindingSource As BindingSource
+    Friend WithEvents CustomersTableAdapter As MMABooksDataSetTableAdapters.CustomersTableAdapter
+    Friend WithEvents TableAdapterManager As MMABooksDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents CustomersBindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents CustomersBindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents CustomerIDTextBox As TextBox
+    Friend WithEvents NameTextBox As TextBox
+    Friend WithEvents AddressTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents ZipCodeTextBox As TextBox
+End Class
---
+++ CustomerMaintenance.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+
+Public Class CustomerMaintenance
+    Private Sub CustomerMaintenance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'MMABooksDataSet.Customers' table. You can move, or remove it, as needed.
+        Me.CustomersTableAdapter.Fill(Me.MMABooksDataSet.Customers)
+
+    End Sub
+    Private Sub CustomersBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles CustomersBindingNavigatorSaveItem.Click
+        ' ADO.NET DataException
+        Try
+            Me.CustomersBindingSource.EndEdit()
+            Me.TableAdapterManager.UpdateAll(Me.MMABooksDataSet)
+        Catch ex As DBConcurrencyException
+            MessageBox.Show("A concurrency error occured. " &
+                   "Some rows were not updated.", "Concurrency Error")
+            Me.CustomersTableAdapter.Fill(Me.MMABooksDataSet.Customers)
+        Catch ex As DataException
+            MessageBox.Show(ex.Message, ex.GetType.ToString)
+            CustomersBindingSource.CancelEdit()
+        Catch ex As SqlException
+            MessageBox.Show("Database error # " & ex.Number &
+                  ": " & ex.Message, ex.GetType.ToString)
+        End Try
+
+        Me.Validate()
+        Me.CustomersBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.MMABooksDataSet)
+
+    End Sub
+End Class
